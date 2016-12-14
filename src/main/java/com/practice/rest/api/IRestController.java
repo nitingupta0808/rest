35f59149cf2cf9e4362e57e37f5e9fd683211068
Nitin Gupta <nitingupta0808@outlook.com>
2016-12-14 23:45:54 +0530

    Third round

--- src/main/java/com/practice/rest/api/IRestController.java
+++ src/main/java/com/practice/rest/api/IRestController.java
@@ -2,42 +2,21 @@ package com.practice.rest.api;
 
 import java.util.List;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
 
 import com.practice.rest.model.Student;
 
-@Path("/student")
 public interface IRestController {
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Student> getAll(@QueryParam("name") String name);
 	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/{rollNumber}")
 	public Student findByRollNumber(@PathParam("rollNumber") int rollNumber);
 	
-	@POST
-	public void deleteByName(@QueryParam("name") String name);
+	public void deleteByName(String name);
 	
-	@DELETE
-	@Path("/{rollNumber}")
 	public void deleteByRollNumber(@PathParam("rollNumber") int rollNumber);
 	
-	@PUT
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("/{rollNumber}")
 	public Student addOrUpdateStudent(@PathParam("rollNumber") int rollNumber, Student student);
 	
 }
--- src/main/java/com/practice/rest/impl/RestController.java
+++ src/main/java/com/practice/rest/impl/RestController.java
@@ -1,11 +1,16 @@
 package com.practice.rest.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.commons.lang.StringUtils;
@@ -17,38 +22,52 @@ import com.practice.service.impl.StudentService;
 
 
 @Path("/student")
-public class RestController {
+public class RestController implements IRestController {
 	
 	
 	private IStudentService studentService;
+	
+	@GET
+	@Path("/test")
+	public String test() {
+		return "Hello World";
+	}	
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Student> getAll() {
-		if(StringUtils.isEmpty("")){
+	public List<Student> getAll(@QueryParam("name")String name) {
+		if(StringUtils.isEmpty(name)){
 			return getStudentService().findAll();
 		}else{
-			return new ArrayList<>();
-			//return getStudentService().findByName(name);	
+			return getStudentService().findByName(name);	
 		}
 	
 	}
 
-	public Student findByRollNumber(int rollNumber) {
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{rollNumber}")
+	public Student findByRollNumber(@PathParam("rollNumber")int rollNumber) {
 		return getStudentService().findById(rollNumber);
 	}
 
-	public void deleteByName(String name) {
+	@POST
+	public void deleteByName(@QueryParam("name")String name) {
+		// TODO Auto-generated method stub
+	}
+
+	@DELETE
+	@Path("/{rollNumber}")
+	public void deleteByRollNumber(@PathParam("rollNumber")int rollNumber) {
 		// TODO Auto-generated method stub
 
 	}
 
-	public void deleteByRollNumber(int rollNumber) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public Student addOrUpdateStudent(int rollNumber, Student student) {
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/{rollNumber}")
+	public Student addOrUpdateStudent(@PathParam("rollNumber")int rollNumber, Student student) {
 		// TODO Auto-generated method stub
 		return null;
 	}
